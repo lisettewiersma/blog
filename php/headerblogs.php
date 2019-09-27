033eba58e446ff2a20c67cd0f0afeb38ab5ca9a6
lisettewiersma <lisette_wiersma@live.nl>
2019-09-27 11:17:25 +0200

    werkende v1

--- php/headerblogs.php
+++ php/headerblogs.php
@@ -1,4 +1,32 @@
 <?php
+// error_reporting(E_ALL);
+
+// //You can also report all errors by using -1
+// error_reporting(-1);
+
+// //If you are feeling old school
+// ini_set('error_reporting', E_ALL);
+
+// function headerblogs() {
+//     include 'connect.php';
+
+//     $newest ="SELECT MAX(id) FROM blogs";
+//     $newpost = $connect->query($newest);
+//     $row1 = fetch_assoc($newpost);
+//     echo $row1['id'];
+//     $headerPost = "SELECT * FROM blogs WHERE id=2";
+//     $result1 = $connect->query($headerPost);
+
+//     while ($row = $result1->fetch_assoc()) {
+//         echo "<div class='headerblog'>";
+//     ///echo "<img src='$row['image']'>";
+//         echo "<h2 class='headertitle'>" . $row['title'] . "</h2> ";
+//         echo "<p>" . $row['author'] . " " . $row['date'] . "</p>" ;
+//         echo "</div>";
+//     }
+//     echo 'hoi';
+// }
+
 error_reporting(E_ALL);
 
 //You can also report all errors by using -1
@@ -8,23 +36,19 @@ error_reporting(-1);
 ini_set('error_reporting', E_ALL);
 
 function headerblogs() {
-    include 'connect.php';
+include 'connect.php';
 
-    $newest ="SELECT MAX(id) FROM blogs";
-    $newpost = $connect->query($newest);
-    $row1 = fetch_assoc($newpost);
-    echo $row1['id'];
-    $headerPost = "SELECT * FROM blogs WHERE id=2";
-    $result1 = $connect->query($headerPost);
 
-    while ($row = $result1->fetch_assoc()) {
-        echo "<div class='headerblog'>";
-    ///echo "<img src='$row['image']'>";
-        echo "<h2 class='headertitle'>" . $row['title'] . "</h2> ";
-        echo "<p>" . $row['author'] . " " . $row['date'] . "</p>" ;
-        echo "</div>";
-    }
-    echo 'hoi';
+$query = "SELECT * FROM blogs WHERE id=(SELECT MAX(id) FROM blogs)";
+$result = $connect->query($query);
+
+while ($row = $result->fetch_assoc()) {
+echo "<div class='headerblog'>";
+// echo "<img src='$row['image']'>";
+echo "<h2 class='headertitle'>" . $row['title'] . "</h2> ";
+echo "<p>" . $row['author'] . " " . $row['date'] . "</p>" ;
+echo "</div>";
+}
 }
 
 ?>
